60f95bec89504c50157711515e289efea7051ebc
Rolandolopez16 <rolandolopez@unicomfacauca.edu.co>
2024-11-05 16:55:07 -0500

    se quita del package.json el valor  type: module

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 
 // Ruta principal de la API
